8bfd56833a2d8a51dc62f9b779e542bc678853a8
Westspray12 <francoisgas0909@gmail.com>
2026-04-28 13:33:19 -0400

    Update main.c

--- software/main.c
+++ software/main.c
@@ -1,1 +1,15 @@
 
+#include <stdio.h>
+#include "vrodux.h"
+
+int main(int argc, char **argv)
+{
+ if (argc < 2)
+ {
+  printf("%sGive a filename.%s\n", BHI_BL, M_RE);
+ }
+ else
+ {
+  printf("%sUndefined error.%s\n", BHI_RE, M_RE);
+ }
+} 
